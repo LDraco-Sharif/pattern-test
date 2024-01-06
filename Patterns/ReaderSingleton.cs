75d6d4b4e36a42c008c47bef157f71e8370cdc14
LDraco-Sharif <shariful.islam.msi47@gmail.com>
2024-01-07 01:40:32 +0600

    Initial implementation of Singleton

---
+++ Patterns/ReaderSingleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_Test.Patterns
+{
+    public class ReaderSingleton
+    {
+        public static ReaderSingleton instance;
+        private List<string> dataList = [];
+        private int count = 0;
+
+        private ReaderSingleton() { }
+
+        public static ReaderSingleton getInstance() { 
+            if(instance == null)
+            {
+                instance = new ReaderSingleton();
+            }  
+            return instance; 
+        }
+
+        public void addData(string data)
+        {
+            this.dataList.Add(data);
+        }
+
+        public void showAll()
+        {
+            foreach(string s in dataList)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Pattern_Test.Patterns;
+
+ReaderSingleton reader = ReaderSingleton.getInstance();
+ReaderSingleton reader2 = ReaderSingleton.getInstance();
+reader.addData("Data");
+reader2.addData("Data 2");
+reader.showAll();
